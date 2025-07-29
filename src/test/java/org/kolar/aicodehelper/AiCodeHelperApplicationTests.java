28214bb6cf6ab2669f4fc45284e5356b89f5182a
吴静文 <935301185@qq.com>
2025-07-29 12:27:25 +0800

    Initial commit: AI programming assistant with Qwen-Max and LangChain4j

---
+++ src/test/java/org/kolar/aicodehelper/AiCodeHelperApplicationTests.java
@@ -0,0 +1,33 @@
+package org.kolar.aicodehelper;
+
+import dev.langchain4j.data.message.ImageContent;
+import dev.langchain4j.data.message.TextContent;
+import dev.langchain4j.data.message.UserMessage;
+import jakarta.annotation.Resource;
+import org.junit.jupiter.api.Test;
+import org.kolar.aicodehelper.ai.AiCodeHelper;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiCodeHelperApplicationTests {
+
+    @Resource
+    private AiCodeHelper aiCodeHelper;
+    @Test
+    void contextLoads() {
+    }
+
+    @Test
+    void chat() {
+        aiCodeHelper.chat("hello im programmer kolar!");
+    }
+
+    @Test
+    void chatWithMessage() {
+        UserMessage userMessage = UserMessage.from(
+                TextContent.from("Describe the following image"),
+                ImageContent.from("https://example.com/cat.jpg")
+        );
+        aiCodeHelper.chatWithMessage(userMessage);
+    }
+}
---
+++ src/test/java/org/kolar/aicodehelper/ai/AiCodeHelper.java
@@ -0,0 +1,36 @@
+package org.kolar.aicodehelper.ai;
+
+import dev.langchain4j.data.message.AiMessage;
+import dev.langchain4j.data.message.UserMessage;
+import dev.langchain4j.model.chat.ChatModel;
+import dev.langchain4j.model.chat.response.ChatResponse;
+import jakarta.annotation.Resource;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+public class AiCodeHelper {
+    @Resource
+    private ChatModel qwenChatModel;
+
+    // 简单对话
+    public String chat(String message) {
+        UserMessage userMessage = UserMessage.from(message);
+        ChatResponse chatResponse = qwenChatModel.chat(userMessage);
+        AiMessage aiMessage = chatResponse.aiMessage();
+        log.info("AI 输出：" + aiMessage.toString());
+        return aiMessage.text();   // ai输出文本
+    }
+
+    // 多模态对话
+    public String chatWithMessage(UserMessage userMessage) {
+        ChatResponse chatResponse = qwenChatModel.chat(userMessage);
+        AiMessage aiMessage = chatResponse.aiMessage();
+        log.info("AI 输出：" + aiMessage.toString());
+        return aiMessage.text();
+    }
+
+
+
+}
